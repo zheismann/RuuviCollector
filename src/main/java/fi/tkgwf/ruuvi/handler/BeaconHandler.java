26da5b0f5cc98b8900a7a82cb3a92204c8a6964f
Zac Heismann <zac.heismann+github.com@gmail.com>
2021-02-21 12:12:20 -0700

    early logging of MAC addresses found

--- src/main/java/fi/tkgwf/ruuvi/handler/BeaconHandler.java
+++ src/main/java/fi/tkgwf/ruuvi/handler/BeaconHandler.java
@@ -1,5 +1,6 @@
 package fi.tkgwf.ruuvi.handler;
 
+import fi.tkgwf.ruuvi.Main;
 import fi.tkgwf.ruuvi.bean.EnhancedRuuviMeasurement;
 import fi.tkgwf.ruuvi.bean.HCIData;
 import fi.tkgwf.ruuvi.common.bean.RuuviMeasurement;
@@ -8,14 +9,22 @@ import fi.tkgwf.ruuvi.common.parser.impl.AnyDataFormatParser;
 import fi.tkgwf.ruuvi.config.Config;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 /**
  * Creates {@link RuuviMeasurement} instances from raw dumps from hcidump.
  */
 public class BeaconHandler {
 
+    private static final Logger LOG = Logger.getLogger(BeaconHandler.class);
     private final DataFormatParser parser = new AnyDataFormatParser();
+    private final Set<String> macAddressesFound = new HashSet<>();
 
     /**
      * Handles a packet and creates a {@link RuuviMeasurement} if the handler
@@ -36,6 +45,10 @@ public class BeaconHandler {
         RuuviMeasurement measurement = parser.parse(adData.dataBytes());
         if (measurement == null) {
             return Optional.empty();
+        }
+        if (!macAddressesFound.contains(hciData.mac)) {
+            macAddressesFound.add(hciData.mac);
+            LOG.debug("Saw MAC for the first time: " + hciData.mac);
         }
         EnhancedRuuviMeasurement enhancedMeasurement = new EnhancedRuuviMeasurement(measurement);
         enhancedMeasurement.setMac(hciData.mac);
